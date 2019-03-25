63d1411d881fca39ee4552821eca6d871cee8484
yannafaith <yannafaith97@gmail.com>
2019-03-25 13:06:53 -0700

    iniatializes deployed server

---
+++ graphql/index.js
@@ -0,0 +1,31 @@
+const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('../prisma/generated/prisma-client');
+
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews clone`,
+        feed: (root, args, context, info) => {
+            return context.prisma.links()
+        },
+    },
+    Mutation: {
+        post: (root, args, context) => {
+            return context.prisma.createLink({
+                url: args.url,
+                description: args.description
+            })
+        },
+    },
+};
+
+const server = new GraphQLServer ({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context: {prisma},
+});
+
+server.start(() => console.log('Server is running on http://localhost:4000'));
+
+
+
+
