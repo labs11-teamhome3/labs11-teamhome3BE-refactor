a49512e748752b7ca8e86aaed6a6357987c0d40c
Clint <clintkunz@gmail.com>
2019-03-26 09:46:32 -0600

    changed development url's to function on my local

--- prisma/generated/prisma-client/index.js
+++ prisma/generated/prisma-client/index.js
@@ -12,6 +12,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `${process.env["PROD_ENDPOINT"]}`
+  endpoint: `http://localhost:4466`
 });
 exports.prisma = new exports.Prisma();
