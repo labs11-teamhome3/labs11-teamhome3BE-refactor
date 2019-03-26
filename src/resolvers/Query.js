cd9665f884be16e4c6323c3a167f74db64963aa5
Daniel Weinman <namniewd@gmail.com>
2019-03-26 17:06:44 -0400

    added queries to get todo or todoList by id

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,9 +1,5 @@
 const info = () => `This is the API of Manaje`
 
-const todoes = (parent, args, context, info) => {
-        return context.prisma.todoes()
-}
-    
 const users = (parent, args, context, info) => {
         return context.prisma.users();
 }
@@ -12,10 +8,23 @@ const todoLists = (parent, args, context, info) => {
         return context.prisma.todoLists();
 }
 
+const todoList = (parent, args, context, info) => {
+        return context.prisma.todoList({ id: args.id })
+}
+
+const todoes = (parent, args, context, info) => {
+        return context.prisma.todoes()
+}
+
+const todo = (parent, args, context, info) => {
+        return context.prisma.todo({ id: args.id })
+}
 
 module.exports = {
     info,
     todoes,
+    todo,
     users,
-    todoLists
+    todoLists,
+    todoList
 }
