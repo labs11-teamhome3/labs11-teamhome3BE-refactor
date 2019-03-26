b6d28dd22501f84e46c409c9a4b05b932ca1abc6
Clint <clintkunz@gmail.com>
2019-03-26 13:42:41 -0600

    add express middleware tooling and connected it

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('../prisma/generated/prisma-client');
 const resolvers = require('./resolvers/index');
+const configMiddleware = require('./config/express-middleware');
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
     context: {prisma}
 })
+
+configMiddleware(server); 
+
 server.start(() => console.log(`Server is running on http://localhost:4000`));
 
