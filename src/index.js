2b83298dcc2e62b4039ffcb24bac30b68df1eed7
Kevin Sooter <kevin.m.sooter@gmail.com>
2019-03-27 18:00:17 -0500

    Added teamId's for hooking up the front end

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 //require('dotenv').config();
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('../prisma/generated/prisma-client');
-const resolvers = require('./resolvers/index');;
-
+const resolvers = require('./resolvers/index');
 
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers,
-    context: {prisma}
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`));
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: { prisma },
+});
 
+server.start(() => console.log(`Server is running on http://localhost:4000`));
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,46 +1,48 @@
-const info = () => `This is the API of Manaje`
+const info = () => `This is the API of Manaje`;
 
 const users = (parent, args, context, info) => {
-        return context.prisma.users();
-}
+  return context.prisma.users();
+};
 
 const user = (parent, args, context, info) => {
-        return context.prisma.user({ id: args.id });
-}
+  return context.prisma.user({ id: args.id });
+};
 
 const todoLists = (parent, args, context, info) => {
-        return context.prisma.todoLists();
-}
+  if (args.teamId) {
+    return context.prisma.todoLists({ where: { inTeam: { id: args.teamId } } });
+  }
+  return context.prisma.todoLists();
+};
 
 const todoList = (parent, args, context, info) => {
-        return context.prisma.todoList({ id: args.id });
-}
+  return context.prisma.todoList({ id: args.id });
+};
 
 const todoes = (parent, args, context, info) => {
-        return context.prisma.todoes();
-}
+  return context.prisma.todoes();
+};
 
 const todo = (parent, args, context, info) => {
-        return context.prisma.todo({ id: args.id });
-}
+  return context.prisma.todo({ id: args.id });
+};
 
 const teams = (parent, args, context, info) => {
-        return context.prisma.teams();
-}
+  return context.prisma.teams();
+};
 
 const team = (parent, args, context, info) => {
-        return context.prisma.team({ id: args.id });
-}
-
+  return context.prisma.team({ id: args.id });
+};
 
 module.exports = {
-    info,
-    todoes,
-    todo,
-    users,
-    user,
-    todoLists,
-    todoList,
-    teams,
-    team
-}
+  info,
+  todoes,
+  todo,
+  users,
+  user,
+  todoLists,
+  todoList,
+  teams,
+  team,
+};
