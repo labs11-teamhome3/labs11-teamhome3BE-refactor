ef7d3ccfabe87c6d1b16036c5e0ca5809a4f4a8b
Clint Kunz <clintkunz@gmail.com>
2019-03-28 14:44:35 -0600

    Auth0 example code, not working yet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 //require('dotenv').config();
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('../prisma/generated/prisma-client');
+
+const jwtCheck = require('./authorization');
 const resolvers = require('./resolvers/index');
 
 const server = new GraphQLServer({
@@ -8,5 +10,7 @@ const server = new GraphQLServer({
   resolvers,
   context: { prisma },
 });
+
+server.express.use(jwtCheck);
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
