00ed7cd264dde9e42164505738f96f857b127310
Daniel Weinman <namniewd@gmail.com>
2019-04-05 11:42:07 -0400

    started writing code handle adding events on BE, but it will be done on front

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -50,17 +50,31 @@ async function updateUser(parent, args, context, info) {
 } */
 
 async function createTodo(parent, args, context, info) {
+    // commented out code is how to create an event on the activity timeline for this action, currently handling event creation on the front end by calling addEvent mutation
+    
     // await context.prisma.createEvent({
-
+    //     action_string: 'created',
+    //     object_string: 'todo',
+    //     user: {
+    //         connect: {
+    //             id: args.userId
+    //         }
+    //     },
+    //     team: {
+    //         connect: {
+    //             id: args.teamId
+    //         }
+    //     }
     // })
-  return context.prisma.createTodo({
-    description: args.description,
-    partOf: {
-      connect: {
-        id: args.partOf,
-      },
-    },
-  });
+
+    return context.prisma.createTodo({
+        description: args.description,
+        partOf: {
+            connect: {
+                id: args.partOf,
+            },
+        },
+    });
 }
 
 async function deleteTodo(parent, args, context, info) {
